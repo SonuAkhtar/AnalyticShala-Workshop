3f65ae48db4890021987fc2b2077b5147780f43d
SonuA <sonua981@gmail.com>
2024-07-17 16:53:48 +0530

    working on success page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,14 @@
 import Homepage from "./pages/Homepage/Homepage";
+import Success from "./pages/Success/Success";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="app">
-      <Homepage />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/success" element={<Success />} />
+      </Routes>
     </div>
   );
 };
