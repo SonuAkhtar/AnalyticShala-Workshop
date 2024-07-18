376beaf3b7c63b7dacf9c51b681e12c0e8d56e76
SonuA <sonua981@gmail.com>
2024-07-18 20:09:53 +0530

    loader created

---
+++ client/src/components/Loader/Loader.jsx
@@ -0,0 +1,11 @@
+import "./loader.css";
+
+const Loader = () => {
+  return (
+    <div className="loader__wrapper">
+      <h2 data-text="Submitting...">Submitting...</h2>
+    </div>
+  );
+};
+
+export default Loader;
